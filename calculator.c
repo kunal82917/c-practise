bfaeec1acec64cd41bcb8ed1e2fbbb9630d44d5d
kunal <kunalgupta9340@gmail.com>
2024-08-04 01:45:03 +0530

    Create calculator.c

---
+++ calculator.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+#include<conio.h>
+int main()
+{
+    int a,b;
+    printf("enter the first digit",a);
+    scanf("%d",&a);
+    printf("enter the second digit",b);
+    scanf("%d",&b);
+    printf("addition of two digits are:%d\n",a+b);
+    printf("subtraction of two digits are:%d\n",a-b);
+    printf("multiplication of two digits are:%d\n",a*b);
+    printf("division of two digits are:%d\n",a/b);
+    getch();
+}
